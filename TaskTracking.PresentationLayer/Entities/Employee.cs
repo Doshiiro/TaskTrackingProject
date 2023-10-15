acc226c34e533ed2421ae129cd446c4cb36d8ff0
Melih Akyıldız <melihakyildiz0206@gmail.com>
2023-10-15 17:55:34 +0300

    employee

---
+++ TaskTracking.PresentationLayer/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracking.PresentationLayer.Entities
+{
+    public class Employee
+    {
+        public int emp_ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
