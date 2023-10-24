c327314fb561136c0a05f0a82877ef68335ebc92
umut <122454421+Umut1406@users.noreply.github.com>
2023-10-24 18:50:16 +0300

    login page form oluşturuldu

---
+++ TaskTracking.PresentationLayer/LoginPageForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskTracking.PresentationLayer
+{
+    public partial class LoginPageForm : Form
+    {
+        public LoginPageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPageForm_Load(object sender, EventArgs e)
+        {
+            //testsdaas
+            
+        }
+    }
+}
